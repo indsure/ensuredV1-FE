689976c123397bfac8e034f7253b40f42291efad
deepakkashyapwebexcellis <deepakkashyap@webexcellis.com>
2026-02-12 22:38:00 +0530

    fix: increase request size limit

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -70,8 +70,8 @@ app.use(
 
 /* ---------------- BODY PARSING ---------------- */
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 /* ---------------- API LOGGER ---------------- */
 
